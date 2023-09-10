947300c583eea9c85ed0c5483fc868754bedf451
Shahin <shahin.shdm@gmail.com>
2023-09-10 13:16:16 +0330

    fix js and index

---
+++ embed.js
@@ -0,0 +1,18 @@
+function PRAvatar_getRandomInt(max) {
+  return 1 + Math.floor(Math.random() * (max-1));
+}
+
+function PRAvatar_showAvatar(width=100,height=100){
+	var id = PRAvatar_getRandomInt(5);
+	var avatar = document.createElement("img");
+	avatar.setAttribute('src', 'https://shahind.github.io/persian-avatar/avatars/'+id+'.jpg');
+	avatar.setAttribute('height', height);
+	avatar.setAttribute('width', width);
+	document.body.appendChild(avatar);
+}
+
+function PRAvatar_feedImg(avatarID){
+	var id = PRAvatar_getRandomInt(5);
+	var avatar = document.getElementById(avatarID);
+	avatar.setAttribute('src', 'https://shahind.github.io/persian-avatar/avatars/'+id+'.jpg');
+}
